4b75d339c17adaf09f5a02866da3f5a5f6eb7a6b
michaelrhodes <spam@michaelrhod.es>
2014-04-12 18:04:08 +1000

    Pretty sure I shouldn’t be freeing that memory…

--- test.c
+++ test.c
@@ -12,11 +12,9 @@ main (void) {
 
   basename = path_basename("/path/to/file.ext", NULL);
   assert(strcmp("file.ext", basename) == 0);
-  free(basename);
 
   basename = path_basename("/path/to/file.ext", ".ext");
   assert(strcmp("file", basename) == 0);
-  free(basename);
 
   return 0;
 }
